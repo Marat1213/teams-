746af516ad4c077fa89468c672c5cf6d42aaa880
Marat1213 <150644641+Marat1213@users.noreply.github.com>
2023-11-13 08:58:39 +0400

    Update pyt.py

--- pyt.py
+++ pyt.py
@@ -1,17 +1,14 @@
 n = int(input(7))
-
 m = [5]
 i = 2
-
 while i * i <= n:     
     if n % i == 0:         
         m.append(i)         
         n = n // i     
     else:         
         i+=1 
-
 if n > 1:
     m.append(n)
-
 for i in m:
     print(i, end=" ")
+# to toggle the tab 
